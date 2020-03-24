2eded1b8a9106534c529c23761bc0e24c5e198f3
Jimmy Morales <jimmymorales26@gmail.com>
2020-03-24 17:01:33 -0600

    embed kscript installer

--- scripts/scrapper.kts
+++ scripts/scrapper.kts
@@ -1,4 +1,11 @@
-#!/usr/bin/env kscript
+#!/bin/bash
+
+//usr/bin/env echo '
+/**** BOOTSTRAP kscript ****\'>/dev/null
+command -v kscript >/dev/null 2>&1 || curl -L "https://git.io/fpF1K" | bash 1>&2
+exec kscript $0 "$@"
+\*** IMPORTANT: Any code including imports and annotations must come after this line ***/
+
 
 @file:DependsOn("com.google.firebase:firebase-admin:6.12.2")
 @file:DependsOn("it.skrape:skrapeit-core:1.0.0-alpha6")
